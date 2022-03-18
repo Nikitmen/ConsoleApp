820265d0bdff738b8a6337d100125c556d0fc7a6
ndopen@mail.ru <ndopen@mail.ru>
2022-03-18 09:24:49 +0300

    Алг,поиск

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,60 +1,29 @@
 ﻿#include <iostream>
-#include <time.h>
 using namespace std;
 
-//функция, сливающая массивы
-void connect(int* a, int first, int last)
-{
-	int middle, start, final, j;
-	int* mas = new int[1001];
-	middle = (first + last) / 2; //вычисление среднего элемента
-	start = first; //начало левой части
-	final = middle + 1; //начало правой части
-	for (j = first; j <= last; j++) //выполнять от начала до конца
-		if ((start <= middle) && ((final > last) || (a[start] < a[final])))
-		{
-			mas[j] = a[start];
-			start++;
-		}
-		else
-		{
-			mas[j] = a[final];
-			final++;
-		}
-	//возвращение результата в список
-	for (j = first; j <= last; j++) a[j] = mas[j];
-	delete[]mas;
-};
-//рекурсивная процедура сортировки
-void Sort(int* a, int first, int last)
-{
-	{
-		if (first < last)
-		{
-			Sort(a, first, (first + last) / 2); //сортировка левой части
-			Sort(a, (first + last) / 2 + 1, last); //сортировка правой части
-			connect(a, first, last); //слияние двух частей
-		}
-	}
-};
-//главная функция
-int main()
-{
-	setlocale(LC_ALL, "Rus");
-	int i, n;
-	cout << "Размер массива: "; cin >> n;
-	int* a = new int[1001];
-	clock_t start, end;
-	start = clock();
-	for (int i = 0; i < n; i++) {
-		a[i] = rand() % 101; // заполняем массив случайными числами
-		cout << a[i] << "  "; // вывод массива на экран
-	}
-	Sort(a, 0, n); //вызов сортирующей процедуры
-	cout << endl; //вывод упорядоченного массива
-	for (i = 1; i <= n; i++) cout << a[i] << "  ";
-	cout << endl;
-	end = clock();
-	printf("Время выполнение программы %.4f\n", ((double)end - start) / ((double)CLOCKS_PER_SEC));
-	delete[]a;
+int main() {
+    setlocale(LC_ALL, "rus");
+    int n, i, j, q;
+    int position = 0;
+    cout << "Введите размер массива: "; cin >> n;
+    int* arr = new int[n]; // создали массив на n элементов
+
+    for (int i = 0; i < n; i++) //Рандом заполнение массива
+    {
+        arr[i] = rand() % 100; // заполняем массив случайными числами
+        cout << arr[i] << "  "; // вывод массива на экран
+    }
+    cout << endl;
+    cout << "Введите число которые надо найти: "; cin >> q;
+    if (arr[n - 1] != q) //Проверим, нет ли искомого элемента на последней позиции
+    {
+        arr[n - 1] = q;  //Установим &quot;барьер&quot;
+        for (; arr[position] != q; position++);  //Поиск
+        position++; //Корректируем номер позиции
+    }
+    else {
+        cout << n;
+        return 0;
+    }
+    cout << "Позиция введенного числа равна: " << position; //Если элемент найден, то возвращаем его позицию, если нет, то 0
 }
