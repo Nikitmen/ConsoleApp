1463b788c64546f6bb40c3b2048b4f38cdf95cfc
ndopen@mail.ru <ndopen@mail.ru>
2022-03-25 09:23:01 +0300

    Pr_6

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "rus");
-    int n, i, j, q;
-    int position = 0;
+    int key, n, i, j, buff;
     cout << "Введите размер массива: "; cin >> n;
     int* arr = new int[n]; // создали массив на n элементов
 
@@ -14,16 +13,37 @@ int main() {
         cout << arr[i] << "  "; // вывод массива на экран
     }
     cout << endl;
-    cout << "Введите число которые надо найти: "; cin >> q;
-    if (arr[n - 1] != q) //Проверим, нет ли искомого элемента на последней позиции
+    //Сортировка начало
+    for (i = 1; i < n; i++)
     {
-        arr[n - 1] = q;  //Установим &quot;барьер&quot;
-        for (; arr[position] != q; position++);  //Поиск
-        position++; //Корректируем номер позиции
+        buff = arr[i];
+        for (j = i - 1; j >= 0 && arr[j] > buff; j--)
+            arr[j + 1] = arr[j];
+
+        arr[j + 1] = buff;
     }
-    else {
-        cout << n;
-        return 0;
+    //Сортировка конец
+    cout << "Отсортированный массив" << endl;//Сорутируем массив для того чтобы сработал поиск,тк для него нужен отср массив
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << "  ";
     }
-    cout << "Позиция введенного числа равна: " << position; //Если элемент найден, то возвращаем его позицию, если нет, то 0
+    cout << endl << "Введите ключ: ";
+
+    cin >> key; // считываем ключ
+
+    bool flag = false;
+    int l = 0; // левая граница
+    int r = n - 1; // правая граница
+    int mid;
+
+    while ((l <= r) && (flag != true)) {
+        mid = (l + r) / 2; // считываем срединный индекс отрезка [l,r]
+
+        if (arr[mid] == key) flag = true; //проверяем ключ со серединным элементом
+        if (arr[mid] > key) r = mid - 1; // проверяем, какую часть нужно отбросить
+        else l = mid + 1;
+    }
+
+    if (flag) cout << "Индекс элемента " << key << " в массиве равен: " << mid + 1 << endl;
+    return 0;
 }
